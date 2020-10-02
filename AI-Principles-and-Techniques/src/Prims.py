c92303e259b3452529aee9b66df62d7191bb045c
Jordy Aaldering <aalderingjordy@gmail.com>
2020-10-02 15:49:07 +0200

    Cleanup

--- AI-Principles-and-Techniques/src/Prims.py
+++ AI-Principles-and-Techniques/src/Prims.py
@@ -1,41 +1,49 @@
 from typing import List
+
 from sys import maxsize as Inf
 
-def extract_min(keys: List[int], in_mst: List[bool]) -> int:
-    min = Inf
-    min_index = -1
- 
-    for v in range(len(keys)):
-        if keys[v] < min and not in_mst[v]:
-            min = keys[v]
-            min_index = v
- 
-    in_mst[min_index] = True
-    return min_index
+class Prims:
 
-def print_mst(graph, parents):
-    print("Edge\t Weight")
-    for i in range(1, len(parents)):
-        print(parents[i], "-", i, "\t", graph[i][parents[i]])
+    Graph = List[List[int]]
 
-def prims(graph: List[List[int]]):
-    size = len(graph)
-    keys = [Inf] * size
-    parents = [None] * size
-    in_mst = [False] * size
- 
-    keys[0] = 0
-    parents[0] = -1
- 
-    for _ in range(size):
-        u = extract_min(keys, in_mst)
+    def __init__(self, graph: Graph):
+        self.graph = graph
+        self.size = len(graph)
 
-        for v in range(size):
-            if graph[u][v] > 0 and not in_mst[v] and keys[v] > graph[u][v]:
-                keys[v] = graph[u][v]
-                parents[v] = u
+        self.keys = [Inf] * self.size
+        self.parents = [None] * self.size
+        self.in_mst = [False] * self.size
+        
+        self.calculate_mst()
+        
+    def calculate_mst(self):
+        self.keys[0] = 0
+        self.parents[0] = -1
  
-    print_mst(graph, parents)
+        for _ in range(self.size):
+            u = self.extract_min()
+
+            for v in range(self.size):
+                if not self.in_mst[v] and self.graph[u][v] > 0 and self.keys[v] > self.graph[u][v]:
+                    self.keys[v] = self.graph[u][v]
+                    self.parents[v] = u
+
+    def extract_min(self) -> int:
+        min = Inf
+        min_index = -1
+ 
+        for v in range(self.size):
+            if not self.in_mst[v] and self.keys[v] < min:
+                min = self.keys[v]
+                min_index = v
+ 
+        self.in_mst[min_index] = True
+        return min_index
+
+    def print_mst(self) -> None:
+        print("Edge\t Weight")
+        for i in range(1, self.size):
+            print(self.parents[i], "-", i, "\t", self.graph[i][self.parents[i]])
 
 if __name__ == "__main__":
     graph = [
@@ -45,4 +53,6 @@ if __name__ == "__main__":
         [0, 0, 4, 0, 3],
         [0, 0, 1, 3, 0],
     ]
-    prims(graph)
+
+    p = Prims(graph);
+    p.print_mst()
