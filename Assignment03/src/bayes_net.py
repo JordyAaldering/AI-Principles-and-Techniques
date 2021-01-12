dad29d15e9361a59a6a8ee1cac3100da931aaa8f
Jordy Aaldering <aalderingjordy@gmail.com>
2021-01-12 14:06:29 +0100

    Basic factor math

--- Assignment03/src/bayes_net.py
+++ Assignment03/src/bayes_net.py
@@ -1,3 +1,4 @@
+import itertools
 import json
 
 class BayesNet():
@@ -22,14 +23,57 @@ class BayesNet():
         print("Variables topologically sorted:", vars_sorted)
         return vars_sorted
     
-    def probability(self, variable, evidence):
-        parents = self.net[variable]["parents"]
+    def probability(self, var, evidence):
+        parents = self.net[var]["parents"]
         if len(parents) == 0:
-            prob = self.net[variable][prob]
+            prob = self.net[var][prob]
         else:
             key = str([evidence[p] for p in parents])
-            prob = self.net[variable]["prob"][key]
+            prob = self.net[var]["prob"][key]
         
-        prob = prob if evidence[variable] else 1 - prob
-        print(f"Variable {variable} with evidence {evidence} has probability: {prob}")
+        prob = prob if evidence[var] else 1 - prob
+        print(f"Variable {var} with evidence {evidence} has probability: {prob}")
         return prob
+    
+    def get_permutations(self, size):
+        permutations = []
+        for comb in itertools.combinations_with_replacement([True, False], size):
+            for perm in itertools.permutations(comb):
+                if perm not in permutations:
+                    permutations.append(perm)
+        return permutations
+
+    def make_factor(self, var, factors, evidence):
+        variables = factors[var]
+        variables.sort()
+
+        parents = [p for p in self.net[var]["parents"]]
+        parents.append(var)
+
+        permutations = self.get_permutations(len(parents))
+
+        asg = {}
+        entries = {}
+        for perm in permutations:
+            violate = False
+            for key, val in zip(parents, perm):
+                if key in evidence and evidence[key] != val:
+                    violate = True
+                    break
+
+                asg[key] = val
+            
+            if violate:
+                continue
+
+            key = tuple(asg[v] for v in variables)
+            prob = self.probability(var, asg)
+            entries[key] = prob
+        
+        return variables, entries
+    
+    def factor_pointwise(self, var, factor1, factor2):
+        pass
+
+    def factor_sumout(self, var, factors):
+        pass
