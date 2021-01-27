eba9fc218d8f498bb3e69a6c8d96ab2ddda977ac
Jordy Aaldering <aalderingjordy@gmail.com>
2021-01-27 20:04:09 +0100

    Cleanup

--- Assignment03/src/bayes_net.py
+++ Assignment03/src/bayes_net.py
@@ -6,38 +6,36 @@ class BayesNet():
         with open(path) as f:
             self.net = json.load(f)
 
-    def probability(self, var, evidence):
-        parents = self.net[var]["parents"]
+    def probability(self, query, evidence):
+        parents = self.net[query]["parents"]
         key = str([evidence[p] for p in parents])
-        prob = self.net[var]["prob"][key]
-        return prob if evidence[var] else 1 - prob
+        prob = self.net[query]["prob"][key]
+        return prob if evidence[query] else 1 - prob
     
-    def eliminate(self, var, evidence):
+    def eliminate(self, query, evidence):
         eliminated = set()
         factors = []
 
         while len(eliminated) < len(self.net):
-            variables = [v for v in list(self.net.keys()) if v not in eliminated]
-            variables = [v for v in variables if all(c in eliminated for c in self.net[v]["children"])]
+            variables = [v for v in list(self.net.keys()) if v not in eliminated and
+                all(c in eliminated for c in self.net[v]["children"])]
 
             factor_vars = {}
             for v in variables:
-                factor_vars[v] = [p for p in self.net[v]["parents"] if p not in evidence]
-                if v not in evidence:
-                    factor_vars[v].append(v)
+                factor_vars[v] = [p for p in self.net[v]["parents"] + [v] if p not in evidence]
             
-            v = sorted(factor_vars.keys(), key=(lambda x: (len(factor_vars[x]), x)))[0]
-            if len(factor_vars[v]) > 0:
-                factors.append(Factor.make(self, v, factor_vars, evidence))
+            var = sorted(factor_vars.keys(), key=(lambda x: (len(factor_vars[x]), x)))[0]
+            if len(factor_vars[var]) > 0:
+                factors.append(Factor.make(self, var, factor_vars, evidence))
             
-            if v != var and v not in evidence:
-                factors = Factor.sumout(v, factors)
+            if var != query:
+                factors = Factor.sumout(var, factors)
 
-            eliminated.add(v)
+            eliminated.add(var)
 
         result = factors[0]
         for f in factors[1:]:
-            result = Factor.pointwise(var, result, f)
+            result = Factor.pointwise(query, result, f)
         
         dist = result[1][(False,)], result[1][(True,)]
         return tuple(x * 1 / sum(dist) for x in dist)
