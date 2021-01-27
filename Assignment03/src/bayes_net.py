2886f9d321ab70c80c169fca80b2cad4efe33d89
Jordy Aaldering <aalderingjordy@gmail.com>
2021-01-27 22:07:04 +0100

    Documentation

--- Assignment03/src/bayes_net.py
+++ Assignment03/src/bayes_net.py
@@ -3,20 +3,39 @@ import json
 
 class BayesNet():
     def __init__(self, path):
+        '''
+        Initializes the network represented as a dictionary.
+        This dictionary maps variable names to:
+            a list of its parents
+            a list of its children
+            a dictionary containing possible value assignments
+                and their corresponding probabilities
+        '''
         with open(path) as f:
             self.net = json.load(f)
 
     def probability(self, query, evidence):
+        '''
+        Returns the probability that the query variable is true,
+        given the evidence set containing variables and their values.
+        This is the same as P(query | evidence).
+        '''
         parents = self.net[query]["parents"]
         key = str([evidence[p] for p in parents])
         prob = self.net[query]["prob"][key]
         return prob if evidence[query] else 1 - prob
     
     def eliminate(self, query, evidence):
+        '''
+        Returns the distribution over the query variable using variable elimination,
+        given the evidence set containing variables and their values.
+        '''
         eliminated = set()
         factors = []
 
         while len(eliminated) < len(self.net):
+            # filter variables that have been eliminated
+            # or whose children have been eliminated
             variables = [v for v in list(self.net.keys()) if v not in eliminated and
                 all(c in eliminated for c in self.net[v]["children"])]
 
@@ -24,18 +43,23 @@ class BayesNet():
             for v in variables:
                 factor_vars[v] = [p for p in self.net[v]["parents"] + [v] if p not in evidence]
             
+            # sort depending on number of variables in each factor
             var = sorted(factor_vars.keys(), key=(lambda x: (len(factor_vars[x]), x)))[0]
+            
             if len(factor_vars[var]) > 0:
                 factors.append(Factor.make(self, var, factor_vars, evidence))
             
+            # if the variable is hidden then sum it out
             if var != query:
                 factors = Factor.sumout(var, factors)
 
             eliminated.add(var)
 
+        # calculate pointwise product
         result = factors[0]
         for f in factors[1:]:
             result = Factor.pointwise(query, result, f)
         
+        # normalize result
         dist = result[1][(False,)], result[1][(True,)]
         return tuple(x * 1 / sum(dist) for x in dist)
--- Assignment03/src/factor.py
+++ Assignment03/src/factor.py
@@ -11,6 +11,12 @@ def get_permutations(size):
 class Factor():
     @staticmethod
     def make(bayes_net, var, factors, evidence):
+        '''
+        Makes a new factor containing:
+            a list of variables
+            a dictionary mapping from possible states
+                to their corresponding probabilities
+        '''
         variables = sorted(factors[var])
         parents = deepcopy(bayes_net.net[var]["parents"]) + [var]
 
@@ -34,7 +40,6 @@ class Factor():
     def pointwise(var, factor1, factor2):
         new_vars = set(factor1[0])
         new_vars.update(factor2[0])
-        new_vars = sorted(list(new_vars))
 
         asg = {}
         table = {}
@@ -42,39 +47,42 @@ class Factor():
             for key, val in zip(new_vars, perm):
                 asg[key] = val
             
-            key = tuple(asg[v] for v in new_vars)
             key1 = tuple(asg[v] for v in factor1[0])
             key2 = tuple(asg[v] for v in factor2[0])
             prob = factor1[1][key1] * factor2[1][key2]
+            
+            key = tuple(asg[v] for v in new_vars)
             table[key] = prob
 
         return new_vars, table
 
     @staticmethod
     def sumout(var, factors):
+        var_factors = []
         indices = []
-        pwfactors = []
+
         for i, factor in enumerate(factors):
             if var in factor[0]:
-                pwfactors.append(factor)
+                var_factors.append(factor)
                 indices.append(i)
         
-        if len(pwfactors) > 1:
+        if len(var_factors) > 1:
             for i in reversed(indices):
                 del factors[i]
             
-            result = pwfactors[0]
-            for factor in pwfactors[1:]:
+            result = var_factors[0]
+            for factor in var_factors[1:]:
                 result = Factor.pointwise(var, result, factor)
 
             factors.append(result)
 
         for i, factor in enumerate(factors):
+            # enumerate over all hidden variables of the factor
             for j, v in enumerate(factor[0]):
                 if v == var:
-                    newvariables = factor[0][:j] + factor[0][j+1:]
-
+                    new_variables = factor[0][:j] + factor[0][j+1:]
                     new_entries = {}
+
                     for entry in factor[1]:
                         entry = list(entry)
                         newkey = tuple(entry[:j] + entry[j+1:])
@@ -87,8 +95,9 @@ class Factor():
                         
                         new_entries[newkey] = prob
 
-                    factors[i] = (newvariables, new_entries)
-                    if len(newvariables) == 0:
+                    # replace the old factor
+                    factors[i] = (new_variables, new_entries)
+                    if len(new_variables) == 0:
                         del factors[i]
         
         return factors
